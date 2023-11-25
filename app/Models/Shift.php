65950328e3f604923aedc9d1bf57a24bf129e1be
TKSG145180 <zhuzhonzhiyu@gmail.com>
2023-11-25 22:45:06 +0900

    投稿の更新・投稿の削除

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
     use HasFactory;
+    use SoftDeletes;
       Protected $fillable = [
         'user_id',
         'date',
@@ -16,7 +18,7 @@ class Shift extends Model
         ];
         
         
-    public function users(){
+    public function user(){
         return $this->belongsTo(User::class);
     }
      public function getPaginateByLimit(int $limit_count = 10)
--- resources/views/posts/management.blade.php
+++ resources/views/posts/management.blade.php
@@ -5,26 +5,61 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      <style>  
+       table{
+          border:3px black solid;　
+          border-collapse: collapse;　
+         }
+        th,td{
+          border:1px black solid;
+        }
+    </style>    
     </head>
     <body>
         <h1>希望シフト</h1>
-         <form action="/posts" method="POST">
+         <h2 id="table">シフト管理</h2>
+        
             @foreach($shifts as $shift)
-             <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333;">
-            <div>
-                <p>{{ $shift->user_id }}</p>
-                <p>日時{{ $shift->date }}</p>
-                <p>開始時刻{{ $shift->start_time }}  終了時刻{{ $shift->end_time }}</p>
-                <p>
-                    <a href='/posts/list'>承認</a>
-                    <a href='/posts/management'>拒否</a>
-                </p>
-            </div>
-          </div>
-
-            @endforeach
+                <table>
+    			    <thead>
+    			        <tr>
+    			            <th>ユーザーid</th>
+    			            <th>名前</th>
+    			            <th>日時</th>
+    			            <th>開始時刻</th>
+    			            <th>終了時刻</th>
+    			        </tr>
+    			    </thead>
+    			    <tbody>
+    			        <tr>
+    			            <td>{{ $shift->user_id }}</td>
+    			            <td>{{ $shift->user->name }}</td>
+    			            <td>日時{{ $shift->date }}</td>
+    			            <td>開始時刻{{ $shift->start_time }}</td>
+    			            <td>終了時刻{{ $shift->end_time }}</td>
+    			        </tr>
+                    </tbody>
+                </table>
+                <button type="/posts/list">承認</button>
+                <form action="/posts/{{ $shift->id }}" id="form_{{ $shift->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $shift->id }})">delete</button> 
+                </form>
+                <a href='/posts/{{$shift->id}}/edit'>変更</a>
+                @endforeach
+             <script>
+                function deletePost(id) {
+                    'use strict'
+            
+                    if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                        document.getElementById(`form_${id}`).submit();
+                    }
+                }
+            </script>
         <div class='paginate'>
             {{ $shifts->links() }}
         </div>
+        <p><a href='/'>ホームへ戻る</a></p>
     </body>
 </html>
